975d419cd60bcddb8fbca90e4058178f3fbc4961
frank-chris <chrisfrancischris@gmail.com>
2025-07-31 21:59:55 -0700

    add github action, add 404 page and update images

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { fileURLToPath, URL } from 'node:url'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/', // No base path needed for username.github.io repos
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
